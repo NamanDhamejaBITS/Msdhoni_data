3cb24cd60ed615a932836dec011a2e552a0cbcb8
NamanDhamejaBITS <90816053+NamanDhamejaBITS@users.noreply.github.com>
2022-01-14 23:10:28 +0530

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -12,6 +12,8 @@ for i in range(len(data)):
     data.odi_number[i]=int(str(data.odi_number[i])[6:])
 
 print(data.odi_number.head(5))
+
+# Information about the player from the data provided
 ru=s4=s6=sr=0
 for i in range(0, len(data)):
     ru=ru+int(data["runs_scored"][i])
@@ -20,12 +22,14 @@ for i in range(0, len(data)):
     s6=s6+int(data["sixes"][i])
 print(f"Runs:{ru}\nFours:{s4}\nSixes:{s6}\nAverage Strike rate={sr/len(data)}\nMatches Batted in:{len(data)}")
 
+# Representing data using pie chart
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.pie(np.array([s4*4,s6*6,ru-s4*4-s6*6]),labels=["4s","6s","Runs w/o 4s and 6s"],normalize=True,explode = [0.1,0.2,0],autopct='%1.0f%%')
 plt.show()
 
+# Filtering data w.r.t to opposition
 ru=s4=s6=0
 opp=str(input("Enter the opposition name: "))
 for i in range(0, len(data)):
@@ -39,6 +43,7 @@ print(f"Runs:{ru}\nFours:{s4}\nSixes:{s6}")
 plt.pie(np.array([s4*4,s6*6,ru-s4*4-s6*6]),labels=["4s","6s","Runs w/o 4s and 6s"],normalize=True,explode = [0.1,0.2,0],autopct='%1.0f%%')
 plt.show()
 
+# Filtering data w.r.t Ground 
 ru=s4=s6=0
 gnd=str(input("Enter the ground name: "))
 for i in range(0, len(data)):
